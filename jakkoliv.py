346d5c997504674869062366920b9aa141966d63
Štěpán Chomyn <stepanchomyn@seznam.cz>
2023-11-07 11:10:53 +0100

    third commit

--- jakkoliv.py
+++ jakkoliv.py
@@ -1,4 +1,10 @@
 import random
+import string
 
-juhuslik_number = random.randint(1, 100)
-print("Juhuslik number: ", juhuslik_number)
+def generate_random_string(length):
+    characters = string.ascii_letters + string.digits
+    return ''.join(random.choice(characters) for _ in range(length))
+
+random_string = generate_random_string(10)  # Change the length as needed
+print("Random string:", random_string)
+r: ", juhuslik_number)
